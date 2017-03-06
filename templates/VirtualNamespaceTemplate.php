caea105a89e1a3c9952c777640704b02f95f4742
Robert Blank <robert.blank@oxid-esales.com>
2017-03-06 16:49:44 +0100

    ESDEV-4301 Add @internal tag to template

--- templates/VirtualNamespaceTemplate.php
+++ templates/VirtualNamespaceTemplate.php
@@ -6,8 +6,11 @@ namespace OxidEsales\Eshop/* ADD_EDITION_HERE */\Core\Autoload;
 /**
  * This file holds the mapping of classes from the virtual namespace to the concrete classes of each edition.
  * Each edition has its own map file. The map files will be merged like this: CE <- PE <- EE
- * So the mapping to a concrete class of the OXID eShop communitiy edition will be overwritten, if this class exists the
+ * So the mapping to a concrete class of the OXID eShop community edition will be overwritten, if this class exists the
  * PE or EE edition.
+ *
+ * @internal Do not make a module extension for this class.
+ * @see      http://oxidforge.org/en/core-oxid-eshop-classes-must-not-be-extended.html
  */
 class VirtualNameSpaceClassMap/* ADD_EXTENDS_PARENT_EDITION_HERE */
 {
